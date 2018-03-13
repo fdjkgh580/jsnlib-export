a9e9b2a23c74108356f7722a669486143d1dcaee
fdjkgh580 <fdjkgh580@yahoo.com.tw>
2018-03-13 16:22:53 +0800

    -

--- Demo/CSV.php
+++ Demo/CSV.php
@@ -2,13 +2,13 @@
 require_once '../vendor/autoload.php';
 
 //CSV
-$result = Jsnlib\Export::csv(new Jsnlib\Ao(
+$result = \Jsnlib\Export::csv(new \Jsnlib\Ao(
 [
     'TitleInfo' => ['編號', '姓名', '電話'],
     'ContentList' => 
     [
-        0 => ['1', '張先生', '0978-235-235'],
-        1 => ['4', '許小姐', '0978-233-111']
+        ['1', '張先生', '0978-235-235'],
+        ['4', '許小姐', '0978-233-111']
     ],
     'return_map' => true,
 ]));
